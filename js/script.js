05c2bfeff0e062947059c334c4362c15184ab651
NotSoBlue <32020442+NotSoBlue@users.noreply.github.com>
2017-09-23 07:36:32 +0800

    noteGenerator done!

--- js/script.js
+++ js/script.js
@@ -22,16 +22,47 @@ var bgColor = [
 ];
 
 
-var note = {
-	id: 0,
-	color: 0,
-	date: "9/22/2017",
 
+var sampleNote = [
+				{
+					id: 1,
+					position: 1,
+					color: 0,
+					important: false,
+					title: "wololo",
+					date: "9/22/2017",
+					content:"hello guyz",
+				},	{
+					id: 1,
+					position: 1,
+					color: 3,
+					important: true,
+					title: "Title goes here",
+					date: "9/22/2017",
+					content:"hi mudafukas <br> hello world",
+				},
+	];
 
+function generateNoteCard(obj, container = "#note-container") {
+	var noteContainer = $(container);
+	var favorite = "";
+	var noteContent = "";
+	var stringHolder = ""
+	for(var i in obj) {
+		stringHolder =  "<article class='note-card' data-id='" + obj[i].id + 
+						"' data-position = '" + obj[i].position + 
+						"' data-important='" + obj[i].important + 
+						"' data-color='" + bgColor[obj[i].color] + 
+						"'><header class='note-header'><h1 class='note-title'>" + obj[i].title + 
+						"<br></h1><h4 class='note-date'>" + obj[i].date + 
+						"</h4></header><div class='note-content'><p>" + obj[i].content + 
+						"</p></div></article>";
+		if (obj[i].important) favorite 	+= stringHolder;
+		else 			noteContent 	+= stringHolder;
+	}
+	noteContainer.html(favorite += noteContent);
+}
 
-
-
-};
 
 $(function(){
 	var noteTitle = $("h1.note-title");
@@ -44,5 +75,7 @@ $(function(){
 							$(this).siblings().removeClass('bt-active')
 							$(this).addClass('bt-active');
 						}
-					});	
+					});
+
+	generateNoteCard(sampleNote);
 });
